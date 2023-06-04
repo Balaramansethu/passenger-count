dc087222e4505bfe4dde35a5b227ca3adda2d44c
Balaramansethu <baalaaraman@gmail.com>
2023-06-04 21:42:53 +0530

    This is my final commit

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,4 +1,15 @@
-// this is js file 
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
 
-const bala = 789999
-console.log(bala)
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent=0
+    count=0
+}
